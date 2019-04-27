849f22969bd3710ad1fb7f9909a3f5f401ad507b
HDwares <49922820+HDwares@users.noreply.github.com>
2019-04-27 23:31:44 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -175,6 +175,177 @@ function crtSybl(lineColor) {
     });
 }
 function capitalizeFirstLetter(string) {
+function openNav() {
+    if (window.innerWidth<=600) {
+        document.getElementById("mySidenav").style.width = "84%";
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "38%";
+    }
+} function closeNav(event) {
+    if (event.target.className != "burgerMenu") {
+        document.getElementById("mySidenav").style.width = "0px";
+    }
+    closeFlyout(event);
+}
+// Hides all elements excluding provided className
+function hideAll() {
+    let frames = document.getElementsByClassName("frame");
+    for (let i = 0;
+        i < frames.length;
+        i++) {
+        frames[i].style.display = "none"
+    }
+    return true;
+}
+function openPage(evt, pageName) {
+    let i, tabcontent, tabLinks;
+    hideAll();
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (let i = 0;
+        i < tabLinks.length;
+        i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    } document.getElementById(pageName).style.display = "block";
+    document.getElementsByClassName("PageTitle")[0].innerHTML = evt.currentTarget.innerHTML;
+    evt.currentTarget.className += " active";
+    if (pageName === "MyWork") {
+        let thumbnails = document.getElementsByClassName("thumbnail");
+        for (let i = 0; i < thumbnails.length; i++) {
+            thumbnails[i].addEventListener("mousemove", (event) => {
+                event.target.style.objectFit = "none";
+                event.target.style.objectPosition = event.offsetX / thumbnails[i].width * 100 + "% " + event.offsetY / thumbnails[i].height * 100 + "%";
+            });
+            thumbnails[i].addEventListener("mouseleave", (event) => {
+                event.target.style.objectFit = "cover";
+                event.target.style.objectPosition = "0 0";
+            });
+        }
+    }
+    else if (pageName === "Contact") {
+        createColoredShadow(document.getElementById("hdSymbol"));
+        contactTiles = document.querySelectorAll("#Contact .tile");
+        for (let i = 0; i < contactTiles.length; i++) {
+            createColoredBorder(contactTiles[i]);
+        }
+    }
+} function modalUI(evt) {
+    let modal = document.getElementById('modalUI');
+    let modalCarousel = document.getElementsByClassName('modal-carousel');
+    let modalDesc = document.getElementsByClassName('modal-desc')[0];
+    let utility;
+    let elName = evt.currentTarget.getAttribute("data-title");
+    for (let i = 0; i < utilities.length; i++) {
+        if (elName.trim() === utilities[i].name.trim()) {
+            utility = utilities[i];
+            break;
+        }
+    }
+    let leftArrow = document.querySelector(".fa-angle-left");
+    let rightArrow = document.querySelector(".fa-angle-right");
+    for (let i = 0; i < utility.imgData.length; i++) {
+        modalCarousel[0].innerHTML += `<img class="modal-content" src="` + 'img/' + utility.name.replace(/ /g, "_") + "/" + utility.imgData[i].fileName + `"/>`//+`" style="transform: scale(` + (utility.imgData.length-i)/utility.imgData.length + `)"/>`;
+    }
+    let modalCntnts = document.getElementsByClassName('modal-content');
+    modalCntnts[0].classList.add("infocus");
+    modalDesc.innerHTML = utility.imgData[0].description;
+    let j = 0;
+    let activateKey = (i) => {
+        if (j === 0) {
+            leftArrow.classList.add("active");
+        }
+        else {
+            leftArrow.classList.remove("active");
+        }
+        if (j == utility.imgData.length - 1) {
+            rightArrow.classList.add("active");
+        }
+        else {
+            rightArrow.classList.remove("active");
+        }
+    }
+    //Image slideshow;
+    leftArrow.addEventListener("click", ()=> addFocus(utility, "leftArrow"));
+    rightArrow.addEventListener("click", ()=> addFocus(utility, "rightArrow"));
+    let modalTitle = document.getElementsByClassName("modalTitle");
+    modal.style.display = "block";
+    modalTitle[0].innerHTML = evt.currentTarget.parentElement.getElementsByTagName("h2")[0].innerHTML;
+    let span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        modal.style.display = "none";
+        modalDesc.innerHTML = "";
+        modalCarousel[0].innerHTML = "";
+        leftArrow.classList.add("active");
+        rightArrow.classList.remove("active");
+    };
+    window.onkeyup = (e) =>{
+        var key = e.which;
+        if(key == 13 || key == 39) { // the enter key code or right arrow
+          rightArrow.click();
+          return false;  
+        } else if(key == 37) { // left arrow
+          leftArrow.click();
+          return false;  
+        }
+      };
+      function addFocus(utility, keyType){
+        (keyType==="leftArrow")?j--:(keyType==="rightArrow")?j++:j;
+        if (j < 0) { j = 0 }
+        else if (j >= utility.imgData.length) { j = utility.imgData.length - 1 }
+        if (j < utility.imgData.length && j >= 0) {
+            modalCarousel[0].innerHTML = modalCarousel[0].innerHTML.replace(/ infocus/g, "");
+            // for (let j=i;j>=0;j--){
+            //     modalCntnts[i].style.transform= "scale("+j/i+")";
+            // }
+            modalCntnts[j].classList.add("infocus");
+            modalDesc.innerHTML = utility.imgData[j].description;
+        }
+        activateKey(j);
+    }
+}
+function crtSybl(lineColor) {
+    let c = document.getElementById("linkCanvas");
+    let ctx = c.getContext("2d");
+    ctx.lineWidth = 19;
+    ctx.strokeStyle = lineColor;
+    if (lineColor === "rgb(12,12,12)") {
+        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.shadowColor = "none";
+    } else {
+        ctx.shadowColor = "rgb(140,100,160)";
+        ctx.shadowBlur = 90;
+    } ctx.moveTo(198, 59);
+    ctx.lineTo(157, 317);
+    ctx.lineTo(105, 193);
+    ctx.lineTo(392, 145);
+    ctx.lineTo(350, 40);
+    ctx.lineTo(296, 369);
+    ctx.moveTo(263, 390);
+    ctx.bezierCurveTo(460, 287, 380, 256, 270, 260);
+    ctx.stroke();
+    return c.toDataURL();
+} function createColoredShadow(element) {
+    let elementBorder = event.target.style.boxShadow;
+    element.addEventListener("mousemove", function (event) {
+        event.target.style.boxShadow = "0px 0px 91px rgb(" + event.offsetX + ", " + (event.offsetY - event.offsetX) + ", " + (192 - event.offsetY) + ")";
+        event.target.style.border = "10px solid rgb(12,12,12)";
+        hdSymbol.src = light;
+    });
+    element.addEventListener("mouseleave", function (event) {
+        event.target.style.boxShadow = "none";
+        event.target.style.border = elementBorder;
+        hdSymbol.src = dark;
+    });
+} function createColoredBorder(element) {
+    let elementBorder = event.target.style.border;
+    element.addEventListener("mousemove", function (event) {
+        element.style.border = ".6rem solid rgb(" + event.offsetX + ", " + (3 * event.offsetY - event.offsetX) + ", " + (192 - 3 * event.offsetY) + ")";
+    });
+    element.addEventListener("mouseleave", function (event) {
+        element.style.border = elementBorder;
+    });
+}
+function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 function crtUtilityTiles() {
